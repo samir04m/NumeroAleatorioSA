7fc65828221e05822df3e9c2267187b36946d599
Samir Mejia <samir4mt@gmail.com>
2018-03-14 22:11:37 -0500

    Se agregaron nuevos detalles de visuales

---
+++ README.md
@@ -0,0 +1,17 @@
+# Número Aleatorio - Random Number
+
+## Descripción del Proyecto 
+
+Esta página web te permite obtener un valor o número aleatorio que se encuentre dentro del intervalo que definas.
+
+## Dependencias
+Para la realización de este proyecto fueron implementadas las siguientes tecnologías:
+* Para el *css* y el *JavaScrip* : **Materialize**
+* Biblioteca de *JavaScrip* : **JQuery**
+* Para los iconos : **FontAwesome**
+
+## Autores
+* **Samir Mejia** - *Desarrollador* - [samir04m en Github](https://github.com/samir04m)
+
+## Licencia
+Licencia estándar para repositorios públicos de [**Github**](https://github.com)
--- js/myScript.js
+++ js/myScript.js
@@ -1,8 +1,28 @@
+$('.modal').modal();
+Materialize.toast('Bienvenido!', 3000, 'rounded') 
+var numeroResultados = 0;
 
 function getRandomNumber() {
 	var minimo = document.getElementById("min").value;
+	if (minimo == '') {
+		document.getElementById("mensaje-modal").innerHTML = "Debe especificar un valor minimo";
+		$('#modal-dialog').modal('open');
+		return;
+	}
+
 	var maximo = document.getElementById("max").value;
-	
+	if (maximo == '') {
+		document.getElementById("mensaje-modal").innerHTML = "Debe especificar un valor maximo";
+		$('#modal-dialog').modal('open');
+		return;
+	}
+
+	if (minimo > maximo || minimo == maximo){
+		document.getElementById("mensaje-modal").innerHTML = "El valor minimo debe ser menor y diferente al maximo";
+		$('#modal-dialog').modal('open');
+		return;
+	}
+
 	while (true) {
 		var numAleatorio = Math.random();  // devolver un número entre 0 y 1
 		numAleatorio = parseInt(numAleatorio*(maximo-minimo+1)+minimo);
@@ -14,7 +34,10 @@ function getRandomNumber() {
 
   	var resultado = document.getElementById("resultado");
   	resultado.innerHTML = numAleatorio;
+  	nuevoResultado();
+  	changeBackgroundColor();
 }
+
 
 document.onkeydown = PulsarTecla;
 
@@ -22,7 +45,31 @@ function PulsarTecla(e) {
     var e = e || event;
     var tecla =  e.keyCode ;
    
-    if (tecla == 13 || tecla == 32){
+    if (tecla == 13){
     	getRandomNumber();
     }
+}
+
+var colors = [
+	"cyan darken-2", "blue darken-2", "blue darken-4", "purple darken-3",  "purple", "red darken-1", "red", "orange lighten-1",  "orange darken-2", "yellow darken-2", "green", "green darken-2", "teal darken-3"
+]
+
+var indiceColor = 0; 
+
+function changeBackgroundColor(){
+
+	if (indiceColor > colors.length-1){
+		indiceColor = 0;
+	}
+
+	document.getElementsByTagName("body")[0].setAttribute("class", colors[indiceColor]);
+	document.getElementById("btn").setAttribute("class", "btn btn-large waves-effect waves-teal pulse tooltipped "+colors[indiceColor]);
+	document.getElementById("div-nav").setAttribute("class", "nav-wrapper "+colors[indiceColor]);
+	document.getElementById("numeroResultados").setAttribute("class", "badge white-text circle "+colors[indiceColor]);
+	indiceColor = indiceColor + 1;
+}
+
+function nuevoResultado(){
+	numeroResultados = numeroResultados + 1;
+	document.getElementById("numeroResultados").innerHTML = numeroResultados;
 }
